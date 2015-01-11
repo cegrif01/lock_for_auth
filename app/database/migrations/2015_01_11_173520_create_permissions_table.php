6eec8662ee5ffe69ae012753ffe02d35e2cfdff7
Charles Griffin <cgriffin@indatus.com>
2015-01-11 12:41:36 -0500

    created permissions table along with permission_role pivot table

---
+++ app/database/migrations/2015_01_11_173520_create_permissions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreatePermissionsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('permissions', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('type', 10);
+			$table->string('action', 100);
+			$table->string('resource_type', 100)->nullable();
+			$table->int('resource_id', 11)->nullable();
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('permissions');
+	}
+
+}
