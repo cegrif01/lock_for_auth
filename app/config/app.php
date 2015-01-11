8956aba5595f44abafb6f513e12ab316051c717e
Charles Griffin <cgriffin@indatus.com>
2015-01-11 12:31:53 -0500

    created roles table

--- app/config/app.php
+++ app/config/app.php
@@ -122,6 +122,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
         'LockDemo\LockOverrides\CustomLockServiceProvider',
+        'Way\Generators\GeneratorsServiceProvider'
 
 	),
 
